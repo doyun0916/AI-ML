5f4ac300abbc871d70386899c87dd105e31b37a7
doyun0916 <52279390+doyun0916@users.noreply.github.com>
2021-02-14 23:38:45 +0900

    5_years_daily_data

---
+++ Stock_Market_prediction/creon_api.py
@@ -0,0 +1,166 @@
+import sys
+from PyQt5.QtWidgets import *
+import win32com.client
+import pandas as pd
+import os
+
+g_objCodeMgr = win32com.client.Dispatch('CpUtil.CpCodeMgr')
+g_objCpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
+
+
+class CpStockChart:
+    def __init__(self):
+        self.objStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
+
+    # 차트 요청 - 기간 기준으로
+    def RequestFromTo(self, code, fromDate, toDate, caller):
+        print(code, fromDate, toDate)
+        # 연결 여부 체크
+        bConnect = g_objCpStatus.IsConnect
+        if (bConnect == 0):
+            print("PLUS가 정상적으로 연결되지 않음. ")
+            return False
+
+        self.objStockChart.SetInputValue(0, code)  # 종목코드
+        self.objStockChart.SetInputValue(1, ord('1'))  # 기간으로 받기
+        self.objStockChart.SetInputValue(2, toDate)  # To 날짜
+        self.objStockChart.SetInputValue(3, fromDate)  # From 날짜
+        # self.objStockChart.SetInputValue(4, 500)  # 최근 500일치
+        self.objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 날짜,시가,고가,저가,종가,거래량
+        self.objStockChart.SetInputValue(6, ord('D'))  # '차트 주기 - 일간 차트 요청
+        self.objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
+        self.objStockChart.BlockRequest()
+
+        rqStatus = self.objStockChart.GetDibStatus()
+        rqRet = self.objStockChart.GetDibMsg1()
+        print("통신상태", rqStatus, rqRet)
+        if rqStatus != 0:
+            exit()
+
+        len = self.objStockChart.GetHeaderValue(3)
+
+        caller.dates = []
+        caller.opens = []
+        caller.highs = []
+        caller.lows = []
+        caller.closes = []
+        caller.vols = []
+        for i in range(len):
+            caller.dates.append(self.objStockChart.GetDataValue(0, i))
+            caller.opens.append(self.objStockChart.GetDataValue(1, i))
+            caller.highs.append(self.objStockChart.GetDataValue(2, i))
+            caller.lows.append(self.objStockChart.GetDataValue(3, i))
+            caller.closes.append(self.objStockChart.GetDataValue(4, i))
+            caller.vols.append(self.objStockChart.GetDataValue(5, i))
+
+        print(len)
+        return caller
+
+    # 차트 요청 - 최근일 부터 개수 기준
+    def RequestDWM(self, code, dwm, count, caller):
+        # 연결 여부 체크
+        bConnect = g_objCpStatus.IsConnect
+        if (bConnect == 0):
+            print("PLUS가 정상적으로 연결되지 않음. ")
+            return False
+
+        self.objStockChart.SetInputValue(0, code)  # 종목코드
+        self.objStockChart.SetInputValue(1, ord('2'))  # 개수로 받기
+        self.objStockChart.SetInputValue(4, count)  # 최근 500일치
+        self.objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 요청항목 - 날짜,시가,고가,저가,종가,거래량
+        self.objStockChart.SetInputValue(6, dwm)  # '차트 주기 - 일/주/월
+        self.objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
+        self.objStockChart.BlockRequest()
+
+        rqStatus = self.objStockChart.GetDibStatus()
+        rqRet = self.objStockChart.GetDibMsg1()
+        print("통신상태", rqStatus, rqRet)
+        if rqStatus != 0:
+            exit()
+
+        len = self.objStockChart.GetHeaderValue(3)
+
+        caller.dates = []
+        caller.opens = []
+        caller.highs = []
+        caller.lows = []
+        caller.closes = []
+        caller.vols = []
+        caller.times = []
+        for i in range(len):
+            caller.dates.append(self.objStockChart.GetDataValue(0, i))
+            caller.opens.append(self.objStockChart.GetDataValue(1, i))
+            caller.highs.append(self.objStockChart.GetDataValue(2, i))
+            caller.lows.append(self.objStockChart.GetDataValue(3, i))
+            caller.closes.append(self.objStockChart.GetDataValue(4, i))
+            caller.vols.append(self.objStockChart.GetDataValue(5, i))
+
+        print(len)
+
+        return
+
+    # 차트 요청 - 분간, 틱 차트
+    def RequestMT(self, code, dwm, count, caller):
+        # 연결 여부 체크
+        bConnect = g_objCpStatus.IsConnect
+        if (bConnect == 0):
+            print("PLUS가 정상적으로 연결되지 않음. ")
+            return False
+
+        self.objStockChart.SetInputValue(0, code)  # 종목코드
+        self.objStockChart.SetInputValue(1, ord('2'))  # 개수로 받기
+        self.objStockChart.SetInputValue(4, count)  # 조회 개수
+        self.objStockChart.SetInputValue(5, [0, 1, 2, 3, 4, 5, 8])  # 요청항목 - 날짜, 시간,시가,고가,저가,종가,거래량
+        self.objStockChart.SetInputValue(6, dwm)  # '차트 주기 - 분/틱
+        self.objStockChart.SetInputValue(7, 1)  # 분틱차트 주기
+        self.objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
+        self.objStockChart.BlockRequest()
+
+        rqStatus = self.objStockChart.GetDibStatus()
+        rqRet = self.objStockChart.GetDibMsg1()
+        print("통신상태", rqStatus, rqRet)
+        if rqStatus != 0:
+            exit()
+
+        len = self.objStockChart.GetHeaderValue(3)
+
+        caller.dates = []
+        caller.opens = []
+        caller.highs = []
+        caller.lows = []
+        caller.closes = []
+        caller.vols = []
+        caller.times = []
+        for i in range(len):
+            caller.dates.append(self.objStockChart.GetDataValue(0, i))
+            caller.times.append(self.objStockChart.GetDataValue(1, i))
+            caller.opens.append(self.objStockChart.GetDataValue(2, i))
+            caller.highs.append(self.objStockChart.GetDataValue(3, i))
+            caller.lows.append(self.objStockChart.GetDataValue(4, i))
+            caller.closes.append(self.objStockChart.GetDataValue(5, i))
+            caller.vols.append(self.objStockChart.GetDataValue(6, i))
+
+        print(len)
+
+        return caller
+
+
+if __name__ == "__main__":
+    class Result:
+        def __init__(self):
+            # 기본 변수들
+            self.dates = []
+            self.opens = []
+            self.highs = []
+            self.lows = []
+            self.closes = []
+            self.vols = []
+            self.times = []
+    new = CpStockChart()
+    final = Result
+    code = input()
+    begins = new.RequestFromTo(code, 20161212, 20210214, final)
+    iter = len(begins.dates)
+    print("코드: ", code)
+    for i in range(iter):
+        print("날짜: ", begins.dates[i], "시가: ", begins.opens[i], "고가: ", begins.highs[i], "저가: ", begins.lows[i], "종가: ", begins.closes[i], "거래량: ", begins.vols[i],)
